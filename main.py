a514a635b2f1c027e25707d1c6aa331fc7460c9a
khunt247 <katiehunt95@gmail.com>
2025-09-06 12:02:42 -0400

    Initial Setup

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Welcome to LockyBear! I am going to help you manage passwords! 🐻”)
+import utils
+
+print (" Welcome to LockyBear! I am going to help you manage passwords! 🐻")
+
+task = input("What would you like to do? (save password or retrieve password)?")
+
+if task == "save password":
+    entity_name = input ("What entity are you saving the password for?")
+    password = input ("What is the password for this?")
+    utils.save_password(entity_name, password)
+    print(f"The password for {entity_name} is saved." )
+
+if task == "retrieve password":
+    entity_name = input ("What entity are you retrieving the password for?")
+    file = entity_name + ".passwd"
+    retrieved_password = utils.retrieve_password(file)
+    print(f"The password for {entity_name} is {retrieved_password}." )
